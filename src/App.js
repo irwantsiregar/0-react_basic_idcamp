623aafc016e9f0665c8b1d533723641506c6e789
Sigadong <irwanto.regar30@gmail.com>
2022-06-15 09:26:25 +0700

    exam using JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import DicodingLogo from './dicoding-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Biodata Perusahaan</h1>
+      <ul>
+        <li>Nama : Dicoding Indonesia</li>
+        <li>Bidang : Education</li>
+        <li>Tagline: Decode Ideas, Discover Potential</li>
+        <img src={DicodingLogo} alt="Dicoding logo" />
+      </ul>
     </div>
   );
 }
