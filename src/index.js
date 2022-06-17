cf54e0739332e06f6afc86f4bcac4c4a9ae1b9fb
Sigadong <irwanto.regar30@gmail.com>
2022-06-18 01:24:42 +0700

    ..

--- src/index.js
+++ src/index.js
@@ -1,39 +1,81 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-class Counter extends React.Component {
+function CounterDisplay({ count }) {
+  if (count === 0) {
+    return <p>{count}</p>;
+  }
+
+  if (count % 5 === 0 && count % 7 === 0) {
+    return <p>FizzBuzz</p>;
+  }
+
+  if (count % 5 === 0) {
+    return <p>Fizz</p>;
+  }
+
+  if (count % 7 === 0) {
+    return <p>Buzz</p>;
+  }
+
+  return <p>{count}</p>;
+}
+
+function IncraseButton({ increase }) {
+  return (
+    <div>
+      <button onClick={increase}>+ increase</button>
+    </div>
+  );
+}
+
+function ResetButton({ reset }) {
+  return (
+    <div>
+      <button onClick={reset}>- reset</button>
+    </div>
+  );
+}
+
+class CounterApp extends React.Component {
   constructor(props) {
     super(props);
 
-    // inisialisasi data dalam state
+    // inisialisasi nilai count di dalam state
     this.state = {
       count: 0
     };
 
-    /*  setState() merupakan fungsi yang digunakan khusus untuk mengubah nilai state di dalam class component. Fungsi inilah yang sebenarnya memicu pemanggilan fungsi render() ketika data di dalam state berubah. Fungsi setState() dapat menerima dua tipe parameter, yaitu objek dan fungsi yang mengembalikan objek 
-        
-    1.parameter berupa objek
-      #reset the count value
-        this.setState({
-          count: 0
-        });
-    
-    2.parameter berupa fungsi
-      #increase the count value
-    */
+    // binding event handler
+    this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind(this);
+    this.onResetEventHandler = this.onResetEventHandler.bind(this);
+  }
+
+  onIncreaseEventHandler() {
     this.setState((previousState) => {
       return {
         count: previousState.count + 1
-      }
+      };
     });
-
+  }
+  onResetEventHandler() {
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    });
   }
 
   render() {
     return (
       <div>
-        {/* menampilkan data count */}
-        <p>{this.state.count}</p>
+        <IncraseButton increase={this.onIncreaseEventHandler} />
+        <CounterDisplay count={this.state.count} />
+        <ResetButton reset={this.onResetEventHandler} />
       </div>
     );
   }
 }
+
+const root = createRoot(document.getElementById('root'));
+root.render(<CounterApp />);
