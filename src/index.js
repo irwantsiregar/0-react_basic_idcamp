bae66bd9d9f66efdc13852b009ace3a6ae99e86f
Sigadong <irwanto.regar30@gmail.com>
2022-06-18 01:05:55 +0700

    Stateful Component | state component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 
-class MyComponent extends React.Component {
+class Counter extends React.Component {
   constructor(props) {
     super(props);
-    console.log('Component created!');
+
+    // inisialisasi data dalam state
+    this.state = {
+      count: 0
+    };
+
+    /*  setState() merupakan fungsi yang digunakan khusus untuk mengubah nilai state di dalam class component. Fungsi inilah yang sebenarnya memicu pemanggilan fungsi render() ketika data di dalam state berubah. Fungsi setState() dapat menerima dua tipe parameter, yaitu objek dan fungsi yang mengembalikan objek 
+        
+    1.parameter berupa objek
+      #reset the count value
+        this.setState({
+          count: 0
+        });
+    
+    2.parameter berupa fungsi
+      #increase the count value
+    */
+    this.setState((previousState) => {
+      return {
+        count: previousState.count + 1
+      }
+    });
+
   }
 
   render() {
-    return <p>Hello, {this.props.name}!</p>;
+    return (
+      <div>
+        {/* menampilkan data count */}
+        <p>{this.state.count}</p>
+      </div>
+    );
   }
 }
-
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <div>
-    <MyComponent name="Dicoding" />
-    <MyComponent name="John" />
-    <MyComponent name="Doe" />
-  </div>
-);
