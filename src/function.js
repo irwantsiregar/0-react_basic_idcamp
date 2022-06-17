c2958bdfa811f24633ee163a421cc1bf1b474db5
Sigadong <irwanto.regar30@gmail.com>
2022-06-18 00:57:47 +0700

    Stateful Component | class component

---
+++ src/function.js
@@ -0,0 +1,11 @@
+// “Stateless” Component
+// Using implementation function component
+import React from 'react';
+
+function MyComponent({ name }) {
+  return (
+    <div>
+      <p>Hello, {name}!</p>
+    </div>
+  );
+}
