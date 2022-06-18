5ca166f5f9ebf7c0f2d278d9e63b4ffe26d37a13
Sigadong <irwanto.regar30@gmail.com>
2022-06-18 13:44:20 +0700

    Stateful Component | case study exercise: adding the add contact feature

--- src/index.js
+++ src/index.js
@@ -1,81 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import ContactApp from './components/ContactApp';
 
-function CounterDisplay({ count }) {
-  if (count === 0) {
-    return <p>{count}</p>;
-  }
+// Styling with Modularization Techniques
+import './styles/style.css';
 
-  if (count % 5 === 0 && count % 7 === 0) {
-    return <p>FizzBuzz</p>;
-  }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ContactApp />
+  </React.StrictMode>
+);
 
-  if (count % 5 === 0) {
-    return <p>Fizz</p>;
-  }
-
-  if (count % 7 === 0) {
-    return <p>Buzz</p>;
-  }
-
-  return <p>{count}</p>;
-}
-
-function IncraseButton({ increase }) {
-  return (
-    <div>
-      <button onClick={increase}>+ increase</button>
-    </div>
-  );
-}
-
-function ResetButton({ reset }) {
-  return (
-    <div>
-      <button onClick={reset}>- reset</button>
-    </div>
-  );
-}
-
-class CounterApp extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // inisialisasi nilai count di dalam state
-    this.state = {
-      count: 0
-    };
-
-    // binding event handler
-    this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind(this);
-    this.onResetEventHandler = this.onResetEventHandler.bind(this);
-  }
-
-  onIncreaseEventHandler() {
-    this.setState((previousState) => {
-      return {
-        count: previousState.count + 1
-      };
-    });
-  }
-  onResetEventHandler() {
-    this.setState(() => {
-      return {
-        count: 0
-      };
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <IncraseButton increase={this.onIncreaseEventHandler} />
-        <CounterDisplay count={this.state.count} />
-        <ResetButton reset={this.onResetEventHandler} />
-      </div>
-    );
-  }
-}
-
-const root = createRoot(document.getElementById('root'));
-root.render(<CounterApp />);
